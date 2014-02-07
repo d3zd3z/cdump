082f1dc743de9c194feb05c7a29ea620444977a3
David Brown <davidb@davidb.org>
2014-02-07 12:29:14 -0800

    Simpler iterator over hash table
    After rereading the docs, it seems that the hash table does support a
    regular iterator over the whole collection, so just use that.

--- src/index.cc
+++ src/index.cc
@@ -13,19 +13,11 @@ FileIndex::SortedIterator::SortedIterator(FileIndex* parent) :parent(parent) {
   keys.reserve(parent->ram.size());
 
   // Build the keys out of the ram index.
-  int count = 0;
-  std::cout << "Buckets: " << parent->ram.bucket_count() << "\n";
-  for (um::size_type bucket = 0; bucket < parent->ram.bucket_count(); ++bucket) {
-    const auto end = parent->ram.end(bucket);
-    for (auto it = parent->ram.begin(bucket); it != end; ++it) {
-      keys.emplace_back(it->first);
-      ++count;
-    }
+  for (auto& it : parent->ram) {
+    keys.emplace_back(it.first);
   }
 
   // Sort all of the keys.
-  std::cout << "Gathered " << count << " items\n";
-  std::cout << "Sorting\n";
   std::sort(keys.begin(), keys.end());
 }
 
